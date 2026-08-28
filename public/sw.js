866813030bf771716b6479c4a745db5a155042f2
Sander Sirio <ssirio96@gmail.com>
2026-08-28 08:48:12 -0300

    sw v65->v66: VRP ja publicado em v65; bump para EPI segregacao + dropdown do painel invalidarem o cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'coleta-v65';
+const CACHE = 'coleta-v66';
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon.png', './logo.png'];
 
 self.addEventListener('install', e => {
